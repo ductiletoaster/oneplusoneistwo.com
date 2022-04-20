dfaf4434843429f64beaf22e44405008e188d5bb
Brian Gebel <bgebel@gofundme.com>
2022-04-19 17:07:17 -0700

    Move to material UI

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Our Wedding Site!
 
 ## TODOs
+[] Install UI framework
+[] https://storybook.js.org/blog/get-started-with-storybook-and-next-js/
 [] Github pages deployment
 [] Automatic deployments on releasing / tagging
 [] CNAME setup
--- site/next.config.js
+++ site/next.config.js
@@ -1,28 +1,6 @@
-// const withPlugins = require('next-compose-plugins');
-// const optimizedImages = require('next-optimized-images');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    disableStaticImages: true,
-  },
   reactStrictMode: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-    }
-  },
 }
 
 module.exports = nextConfig;
-
-// module.exports = withPlugins([
-//   [optimizedImages, {
-//     /* config for next-optimized-images */
-//     optimizeImages: false,
-//   }],
-//   // your other plugins here
-// ], nextConfig);
