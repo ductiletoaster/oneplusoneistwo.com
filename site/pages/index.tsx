83a8266e1fc382e744260a329156e6a16dde9728
Brian Gebel <briangeb@gmail.com>
2022-04-29 21:03:07 -0700

    update index

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,43 +1,56 @@
-
-   
 import * as React from 'react';
 import type { NextPage } from 'next';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Copyright from '../src/Copyright';
 
-const Details: NextPage = () => {
+const Home: NextPage = () => {
   return (
-    <Grid container component="main" sx={{ height: '100vh' }}>
-      <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundColor: '#f1efea',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square >
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography component="h1" variant="h5">
-             YOU'RE INVITED 
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Sticky footer
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {'Pin a footer to the bottom of the viewport.'}
+          {'The footer will move as the main element of the page grows.'}
+        </Typography>
+        <Typography variant="body1">Sticky footer placeholder.</Typography>
+      </Container>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: "#111111"
+        }}
+      >
+          <Box style={{
+            width: "100%",
+            height: "400px",
+            backgroundImage: "url(/nature-scape.svg)",
+            backgroundPosition: "left bottom",
+            backgroundRepeat: "repeat-x",
+            backgroundSize: "contain",
+            position: "fixed",
+            bottom: "64px",
+            left: 0
+          }}/>
+        <Container maxWidth="sm">
+
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
   );
 };
 
-export default Details;
+export default Home;
