7a95951c5c3ba72f9c153e544a31449b2ad82ecf
Brian Gebel <briangeb@gmail.com>
2022-05-10 20:03:11 -0700

    comment

--- site/next.config.js
+++ site/next.config.js
@@ -1,11 +1,8 @@
-// const { i18n } = require('./next-i18next.config');
-// https://dev.to/adrai/static-html-export-with-i18n-compatibility-in-nextjs-8cd
+// @todo get working example of translations
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // @todo re-enable for serveri side rendering
-  // i18n,
   // @todo Can this replace the comonent method?
   // async rewrites() {
   //   return [
